86a4d079a76fed018511cffc273806c31f5df7e2
MuniUday <ch.muniuday6693@gmail.com>
2024-04-02 11:22:53 +0530

    add post call

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
 app.listen(process.env.PORT || 3000);
 console.log('app is listening');
+
+app.use(bodyParser.json());
 
 app.get('/get-app', function (req, res) {
     if (req.query.value) {
@@ -13,4 +16,11 @@ app.get('/get-app', function (req, res) {
 
 app.get('/', function (req, res) {
     res.send('hellow its running');
-})
+});
+
+app.post('/post-app', function (req, res) {
+    if (req.body.message) {
+        console.log(req.body.message);
+    }
+    res.status(200).send(req.body.message);
+});
